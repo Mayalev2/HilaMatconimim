28ab0c3eda0c2a130ee5f739707ff445db820cba
mayalev2 <mayalev224@gmail.com>
2023-12-12 19:01:36 +0200

    c

--- src/App.tsx
+++ src/App.tsx
@@ -42,8 +42,8 @@ export default function App (){
           <Route index element={<Home />} />
           <Route path="Recipe" element={<Recipe />}  />
           <Route path="PhotoGallery" element={<PhotoGallery />}  />
-          <Route path="CahallaWheel" element={<CahallaWheel />}  />
-          <Route path="/matconimim.netlify.app/TodayMeal" element={<TodayMeal />}  />
+          <Route path="/CahallaWheel" element={<CahallaWheel />}  />
+          <Route path="matconimim.netlify.app/TodayMeal" element={<TodayMeal />}  />
         </Route>
       </Routes>
     </BrowserRouter>
