3eff07d0ea3f0ca116871229f69e10fe7b295b78
mayalev2 <mayalev224@gmail.com>
2023-12-14 15:12:57 +0200

    font change

--- src/compenents/ButtonTdM.tsx
+++ src/compenents/ButtonTdM.tsx
@@ -13,8 +13,8 @@ function ButtonTdM ({changeBtn, title, btn1, btn2}:Props){
             <div className='col-12 col-lg-8 d-flex flex-column justify-content-center' style={{direction:"rtl"}}>
             <span className='display-2 text-center mb-5 fw-bold' >{title}</span>
               <div className='col-12 col-lg-9 align-self-center d-flex row justify-content-center'>
-              <button className="button-78 col-6 col-lg-4 align-self-center me-5 ms-5 mb-4 son" style={{fontFamily:`${fontson}`}} role="button" onClick={ () =>{changeBtn(btn1)}}>{btn1}</button>  
-                  <button className="button-78 col-6 col-lg-4 align-self-center me-5 ms-5 mb-4 son" style={{fontFamily:`${fontson}`}} role="button" onClick={ () =>{changeBtn(btn2)}}>{btn2}</button>  
+              <button className="button-78 col-6 col-lg-4 align-self-center me-5 ms-5 mb-4 son" style={{fontFamily:"inherit"}} role="button" onClick={ () =>{changeBtn(btn1)}}>{btn1}</button>  
+                  <button className="button-78 col-6 col-lg-4 align-self-center me-5 ms-5 mb-4 son" style={{fontFamily:"inherit"}} role="button" onClick={ () =>{changeBtn(btn2)}}>{btn2}</button>  
               </div>
           </div>
     </>);
